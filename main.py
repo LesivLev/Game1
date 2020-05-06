860b501e8181df8d60ce2bae292a52375e63de37
Lev Lesiv <lev.a.lesiv@gmail.com>
2020-05-06 21:39:05 +0300

    Добавлены жизни игрока

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ FPS = 60
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (0, 254, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+VP = 25
 
 
 # настройка игрового окна
@@ -25,8 +27,19 @@ WHITE = (255, 255, 255)
 pygame.init() # создание игры
 pygame.mixer.init() # создание (подключение) звуков
 screen = pygame.display.set_mode((WIDTH, HEIGHT)) # создание экрана
-pygame.display.set_caption("World of Tanks") # определяем название игры
+pygame.display.set_caption("Zombe in swamP") # определяем название игры
 clock = pygame.time.Clock()
+
+def draw_shield_bar(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, GREEN, fill_rect)
+    pygame.draw.rect(surf, WHITE, outline_rect, 2)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -39,6 +52,7 @@ class Player(pygame.sprite.Sprite):
         # у rect есть x, y - координата по оси X и по оси Y
         self.rect.centery = HEIGHT / 2
         self.rect.centerx = WIDTH / 2
+        self.hp = 100
         self.speedx = 0 # скокрость передвижения спрайта по оси X
         self.speedy = 0
         
@@ -68,47 +82,165 @@ class Player(pygame.sprite.Sprite):
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
 
-class Zombie(pygame.sprite.Sprite):
+    def hit(self):
+        self.hp -= 1
 
+
+class Zombie(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self) # Определяем переменную как спрайт
         self.image = pygame.Surface((20, 20))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        if(random.randint(0, 2)==0):
-            self.rect.centerx == 10
-        else:
-            self.rect.centerx == HEIGHT-10
-        self.rect.centery =random.randint(0, WIDTH-9)
+        tmp = random.randint(0, 4) 
+        if(tmp == 0):
+            self.rect.centery = 10
+            self.rect.centerx = random.randint(0, WIDTH)
+        elif(tmp == 1):
+            self.rect.centery = HEIGHT-10
+            self.rect.centerx = random.randint(0, WIDTH)
+        elif(tmp == 2):
+            self.rect.centery = random.randint(0, HEIGHT)
+            self.rect.centerx = 10
+        elif(tmp == 3):
+            self.rect.centery = random.randint(0, HEIGHT)
+            self.rect.centerx = WIDTH-10
         self.speedx = 0  
         self.speedy = 0
 
-        
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
 
-# Создать группы спрайтов, чтобы работx`ать с ними со всеми одновременно
+        if(player.rect.x > self.rect.x):
+            self.speedx = 2
+        if(player.rect.x < self.rect.x):
+            self.speedx = -2
+        if(player.rect.x == self.rect.x):
+            self.speedx = 0
+        if(player.rect.y > self.rect.y):
+            self.speedy = 2
+        if(player.rect.y < self.rect.y):
+            self.speedy = -2
+        if(player.rect.y == self.rect.y):
+            self.speedy = 0
+        
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((5, 10))
+        self.image.fill(YELLOW)
+        self.rect=self.image.get_rect()
+        self.rect.centerx = player.rect.x
+        self.rect.centery = player.rect.y
+        self.speedx = 0
+        self.speedy = 0
+
+    def update(self):
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_SPACE]:
+            if(zombie.rect.x > self.rect.x):
+                self.speedx = 2
+            if(zombie.rect.x < self.rect.x):
+                self.speedx = -2
+            if(zombie.rect.x == self.rect.x):
+                self.speedx = 0
+            if(zombie.rect.y > self.rect.y):
+                self.speedy = 2
+            if(zombie.rect.y < self.rect.y):
+                self.speedy = -2
+            if(zombie.rect.y == self.rect.y):
+                self.speedy = 0
+            self.rect.x += self.speedx
+            self.rect.y += self.speedy
+        else:
+            self.rect.x += player.rect.x
+            self.rect.y += player.rect.y
+
+class Coin(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 10))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 350
+        self.rect.centery = 20
+
+class Dog(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) # Определяем переменную как спрайт
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 340
+        self.rect.centery = 340
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+
+        if(zombie.rect.x > self.rect.x):
+            self.speedx = 3
+        if(zombie.rect.x < self.rect.x):
+            self.speedx = -3
+        if(zombie.rect.x == self.rect.x):
+            self.speedx = 0
+        if(zombie.rect.y > self.rect.y):
+            self.speedy = 3
+        if(zombie.rect.y < self.rect.y):
+            self.speedy = -3
+        if(zombie.rect.y == self.rect.y):
+            self.speedy = 0
+        
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+# Создать группы спрайтов, чтобы работать с ними со всеми одновременно
 all_sprites = pygame.sprite.Group()
+all_zombies = pygame.sprite.Group()
 player = Player() # создаем переменную player класса Player
+#dog =  Dog()
+bullet = Bullet()
+#coin = Coin()
+#all_sprites.add(coin)
+all_sprites.add(bullet)
+#all_sprites.add(dog)
 all_sprites.add(player) # добавляем игрока в группу всех спрайтов
 for i in range (0, 3):
     zombie = Zombie()
     all_sprites.add(zombie)
+for i in range (0, 3):
+    zombie = Zombie()
+    all_zombies.add(zombie)
+
 # Игровой цикл
 # 1 - проверить, что цикл работает на правильной частоте обновления кадров
 # 2 - обработка ввода
 # 3 - обновление данных
 # 4 - вывод изменений на экран
 
-run = True # переменная, которая отвечает за запуск игрового цикла
+run = True # переменная, которая отвечает за запуск игрового dикла
 while run:
     clock.tick(FPS) # проверка пункта 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    
+    hits = pygame.sprite.spritecollide(player, all_zombies, False)
+    for hit in hits:
+        player.hit()
+        #if(player.hp <= 0):
+        #    run = False
     all_sprites.update()
 
     screen.fill(BLACK) # заполнение экрана цветом
     all_sprites.draw(screen) # вносим изменения спрайтов на экран
+    draw_shield_bar(screen, 5, 5, player.hp)
     pygame.display.flip() # обновление экран (отображение нового кадра)
 
 pygame.quit()
